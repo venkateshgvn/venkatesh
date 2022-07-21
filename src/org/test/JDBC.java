74b3ed6eacb76c864a0be7b5c1c6fbe879b909c1
venkateshgvn <venkateshdon15@gmail.com>
2022-07-21 15:38:09 +0530

    git new

---
+++ src/org/test/JDBC.java
@@ -0,0 +1,38 @@
+package org.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JDBC {
+	
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		
+		 Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","1754");
+		
+		String s ="select * from employees";
+		
+		PreparedStatement u = connection.prepareStatement(s);
+		
+		ResultSet t = u.executeQuery();
+		
+		while(t.next()) {
+			String n = t.getString("first_name");
+			System.out.println(n);
+		}
+		
+		connection.close();
+		}
+		
+}
+
+
+
+
+	
+
+
