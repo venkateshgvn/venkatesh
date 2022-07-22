30171d842e60669120c6eac8f1ba1d747033e38d
venkateshgvn <venkateshdon15@gmail.com>
2022-07-23 00:55:18 +0530

    vijaycode

--- src/org/test/JDBC.java
+++ src/org/test/JDBC.java
@@ -24,6 +24,8 @@ public class JDBC {
 			String n = t.getString("first_name");
 			String n = t.getString("first_name");
 			System.out.println(n);
+			System.out.println("updated value");
+			System.out.println("updated value3");
 		}
 		
 		connection.close();
